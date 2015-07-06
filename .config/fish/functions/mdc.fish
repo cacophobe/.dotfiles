88eef01310e379298bef53886263ebec8d9b7c1d
cacophobe <cacophobe@users.noreply.github.com>
2015-07-06 17:49:29 +0530

    added fish function to match md5 - mdc

---
+++ .config/fish/functions/mdc.fish
@@ -0,0 +1,11 @@
+function mdc --description 'check md5 with file of same name'
+    set -l md5_of_file (md5 -q $argv | awk '{print tolower($0)}')
+    set -l md5_in_file (cut -d " " -f1 $argv.md5 | awk '{print tolower($0)}')
+    echo "md5 of file: $md5_of_file"
+    echo "md5 in file: $md5_in_file"
+    if test $md5_of_file = $md5_in_file
+        echo "md5 matched"
+    else
+        echo "md5 NOT matching"
+    end
+end
